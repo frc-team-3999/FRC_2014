4f3a78bb4ca28f08cdec824576d13b38891763db
Shadetree <Shadetree@Shadetree-PC>
2014-01-21 19:38:07 -0600

    2014-01-21-19:40
    Changed catapult code for new design.  Need a little more research on encoders, but it basically works.

--- src/edu/wpi/first/wpilibj/templates/Catapult.java
+++ src/edu/wpi/first/wpilibj/templates/Catapult.java
@@ -2,34 +2,42 @@ package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Talon;
 
 public class Catapult implements Runnable {
     
-    Main robot;
-    boolean firing;
+    private final Main robot;
+    public boolean firing;
 
+    // compressor on digital 1 and relay 1
     private static final Compressor compressor = new Compressor(1,1);
-    DoubleSolenoid solenoid = new DoubleSolenoid(1,2);
+    private static final DoubleSolenoid solenoid = new DoubleSolenoid(1,2);
     
-    Controller control;
+    private static final Talon cockingMotor = new Talon(5);
+    private static final Encoder strap = new Encoder(2,3);
+    
+    private final Controller control;
     
     public Catapult(Main robot) {
         this.robot = robot;        
         control = new Controller(2);      
         compressor.start();
-        solenoid.set(DoubleSolenoid.Value.kReverse);
+        solenoid.set(DoubleSolenoid.Value.kForward);
+        strap.setReverseDirection(true);
+        //strap.setSamplesToAverage(5);
     }
     
     public void run() {
         
         new Thread(control).start();
-        
+
         while (true) {
 
             if (control.button[4]) {
                 firing = true;
-                solenoid.set(DoubleSolenoid.Value.kForward);
+                solenoid.set(DoubleSolenoid.Value.kReverse);
                 do {
                     Timer.delay(.5);
                 } while (control.button[4]);
@@ -37,7 +45,7 @@ public class Catapult implements Runnable {
             
             if (control.button[2]) {
                 firing = false;
-                solenoid.set(DoubleSolenoid.Value.kReverse);
+                cock();
                 do {
                     Timer.delay(.5);
                 } while (control.button[2]);
@@ -45,5 +53,23 @@ public class Catapult implements Runnable {
             
             Timer.delay(.1);
         } 
+    }
+    
+    public void cock() {
+        strap.start();
+        strap.reset();
+        while (strap.get() < 100) {
+            Main.station.toLCDLine(4, "Catapult Encoder " + strap.get());
+            cockingMotor.set(.1);
+        }
+        cockingMotor.stopMotor();
+        solenoid.set(DoubleSolenoid.Value.kForward);
+        Timer.delay(1);
+        while (strap.get() > 0) {
+            Main.station.toLCDLine(4, "Catapult Encoder " + strap.get());
+            cockingMotor.set(-.1);
+        }
+        cockingMotor.stopMotor();
+        strap.stop();
     }
 }
